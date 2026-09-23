b978e7ef64b2a77318e32197728340c96772a954
Otuoma Sanya <otuomasp@gmail.com>
2026-09-23 21:06:52 +0300

    correct agriculture

--- src/themes/knbs/app/home-page/home-page.component.ts
+++ src/themes/knbs/app/home-page/home-page.component.ts
@@ -64,7 +64,7 @@ export class HomePageComponent extends BaseComponent {
     'Economic Surveys',
     'Population and Housing Censuses',
     'Consumer Price Indices',
-    'Agricultures',
+    'Agriculture',
     'County Statistical Abstracts',
   ];
 
